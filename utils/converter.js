2cfed266d32c68195b7cd5f1d77936ee425092d0
Kyohei Horikawa <kyohei3430@gmail.com>
2022-01-16 03:39:41 +0900

    ♻️

--- utils/converter.js
+++ utils/converter.js
@@ -21,7 +21,6 @@ const map_after = ["IIII", "VIIII", "XXXX", "LXXXX", "CCCC", "DCCCC"];
 
 export const ArabicToRomanConverter = (props) => {
   let val = Number(props);
-  console.log(val);
   let res = [];
   for (let i = 0; i < arabic.length; i++) {
     const quotient = Math.trunc(val / arabic[i]);
@@ -34,7 +33,7 @@ export const ArabicToRomanConverter = (props) => {
 };
 
 export const RomanToArabicConverter = (props) => {
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < map_after.length; i++) {
     props = props.replace(map_before[i], map_after[i]);
   }
   let res = 0;
@@ -42,6 +41,5 @@ export const RomanToArabicConverter = (props) => {
     const index = roman.indexOf(props.charAt(i));
     res += arabic[index];
   }
-  console.log(res);
   return res;
 };
